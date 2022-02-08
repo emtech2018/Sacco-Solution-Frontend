631c35d67599eb0d2e342c7b9cf15e6af7ca8443
phelly <philndungu@gmail.com>
2022-02-08 17:35:07 +0300

    Commit

--- Branches-Backend/src/main/java/com/emtechhouse/co/ke/Branches/Controllers/BranchController.java
+++ Branches-Backend/src/main/java/com/emtechhouse/co/ke/Branches/Controllers/BranchController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@RestController
 @CrossOrigin
 @RequestMapping("branch")
 public class BranchController {
